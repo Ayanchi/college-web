f7614fc68d05333ea4f9ff1be5499f9a94679fc9
NurdinovE <nurdinovvv06@gmail.com>
2023-02-28 13:42:37 +0600

    start writing routing

--- src/app/firebase.jsx
+++ src/app/firebase.jsx
@@ -12,6 +12,16 @@ const firebaseConfig = {
   measurementId: "G-N1K858RZDF"
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBp9EX7MitBD4vo7suaFAE_f0wJ9sfZeRQ",
+//   authDomain: "startupomania.firebaseapp.com",
+//   projectId: "startupomania",
+//   storageBucket: "startupomania.appspot.com",
+//   messagingSenderId: "236958229930",
+//   appId: "1:236958229930:web:241310f454e4e4ae33b7d3",
+//   measurementId: "G-DMJJ0BGNKN"
+// };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
