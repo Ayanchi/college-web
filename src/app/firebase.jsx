a4aee009eba1b72bbcefbb5367e67b40c8e7046b
pteacher <subfear@mail.ru>
2023-03-13 17:04:28 +0600

    firebase config

--- src/app/firebase.jsx
+++ src/app/firebase.jsx
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnB6wZRnfa8TbG_NDJUsHejO_IqB-DB3Q",
-  authDomain: "college-web-ce60a.firebaseapp.com",
-  projectId: "college-web-ce60a",
-  storageBucket: "college-web-ce60a.appspot.com",
-  messagingSenderId: "1091701822669",
-  appId: "1:1091701822669:web:de4d0cf2b544d16c9c8584",
-  measurementId: "G-N1K858RZDF"
+  apiKey: "AIzaSyBp9EX7MitBD4vo7suaFAE_f0wJ9sfZeRQ",
+  authDomain: "startupomania.firebaseapp.com",
+  projectId: "startupomania",
+  storageBucket: "startupomania.appspot.com",
+  messagingSenderId: "236958229930",
+  appId: "1:236958229930:web:241310f454e4e4ae33b7d3",
+  measurementId: "G-DMJJ0BGNKN"
 };
 
 
