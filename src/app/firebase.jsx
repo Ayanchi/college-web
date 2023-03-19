c1920c72d0fdca85ea16484824d9879a01693449
mrartur0074 <smiteartur01@mail.ru>
2023-03-19 21:58:27 +0600

    fix account

--- src/app/firebase.jsx
+++ src/app/firebase.jsx
@@ -18,7 +18,7 @@ import { getStorage } from "firebase/storage"
 // Erzhany
 
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyDF0bFadz3S5DxrxVmufjqaVI8EAx0l6iU",
   authDomain: "react-firtebase.firebaseapp.com",
   projectId: "react-firtebase",
@@ -26,17 +26,17 @@ const firebaseConfig = {
   messagingSenderId: "570948240941",
   appId: "1:570948240941:web:a1c46ac151c3958b999aa4",
   measurementId: "G-G92GS5798C"
-};
+};*/
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBp9EX7MitBD4vo7suaFAE_f0wJ9sfZeRQ",
-//   authDomain: "startupomania.firebaseapp.com",
-//   projectId: "startupomania",
-//   storageBucket: "startupomania.appspot.com",
-//   messagingSenderId: "236958229930",
-//   appId: "1:236958229930:web:241310f454e4e4ae33b7d3",
-//   measurementId: "G-DMJJ0BGNKN"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBp9EX7MitBD4vo7suaFAE_f0wJ9sfZeRQ",
+  authDomain: "startupomania.firebaseapp.com",
+  projectId: "startupomania",
+  storageBucket: "startupomania.appspot.com",
+  messagingSenderId: "236958229930",
+  appId: "1:236958229930:web:241310f454e4e4ae33b7d3",
+  measurementId: "G-DMJJ0BGNKN"
+};
 
 
 const app = initializeApp(firebaseConfig);
